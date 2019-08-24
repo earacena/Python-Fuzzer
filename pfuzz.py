4dfda45085cbcc5354308d1a13e8d25f234ba4bd
earacena <earacenadev@gmail.com>
2019-08-24 11:15:07 -0400

    Fuzzer now takes filename by argument. Added function that creates randomized strings of specified sizes.

--- pfuzz.py
+++ pfuzz.py
@@ -5,8 +5,34 @@
 # Description: A Python3 fuzzer for finding input that crashes C programs in
 #              in order to generate test cases.
 
+import signal
+import string
+import sys
+import random
+import os
 import subprocess
 from subprocess import Popen, PIPE
 
-process = Popen(['./a.out'], stdin=PIPE, stderr=PIPE)
-stdin, stderr = process.communicate(b'Test\n')
+def generate_random_string(size=10, chars=string.printable):
+    random_str = ''
+    random_str = random_str.join(random.choices(chars, k=size))
+    print(random_str)
+    return random_str
+
+def main():
+    if len(sys.argv) < 2:
+        print ("[-] Error -> Missing executable name: pfuzz.py FILE")
+        return
+    exec_name = str(sys.argv[1])
+    exec_cmd = './' + exec_name
+
+    
+    process = Popen([exec_cmd], stdin=PIPE, stderr=PIPE)
+    gen_str = generate_random_string()
+    enc_gen_str = gen_str.encode('utf8')
+    print(gen_str)
+    print(enc_gen_str)
+    stdin, stderr = process.communicate(enc_gen_str)
+    
+if __name__ == "__main__":
+    main()
